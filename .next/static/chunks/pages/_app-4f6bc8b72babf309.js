b51f733188e467cbbeae2c9131ceb2378ba512e8
T68Bot <t68bot@nobi.ai>
2026-03-20 15:03:36 +0000

    Remove .next build artifacts from repo (again)

--- .next/static/chunks/pages/_app-4f6bc8b72babf309.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{9531:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(1043)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[179],function(){return _(9531),_(9441)}),_N_E=n.O()}]);
